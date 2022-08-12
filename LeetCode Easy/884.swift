335d72d5e2c026efcfdd42a35514274496bdef37
Rodz Amor <ramorSTEM2022@gmail.com>
2022-08-12 02:50:22 -0400

    moved to vscode

--- LeetCode Easy/884.swift
+++ LeetCode Easy/884.swift
@@ -5,4 +5,18 @@
 //  Created by Andrie on 8/12/22.
 //
 
-import Foundation
+class Solution {
+    func uncommonFromSentences(_ s1: String, _ s2: String) -> [String] {
+        let sentence1 = s1.split(separator: " ").map{String($0)}
+        let sentence2 = s2.split(separator: " ").map{String($0)}
+        // return sentence1.symmetricDifference(sentence2).map{String($0)}
+        var words = [String: Int]()
+        for word in sentence1 {
+            words[word, default: 0] += 1
+        }
+        for word in sentence2 {
+            words[word, default: 0] += 1
+        }
+        return Array(words.filter{$0.1 == 1}.keys)   
+    }
+}
--- main.swift
+++ main.swift
@@ -4,9 +4,18 @@
 //
 //  Created by Andrie on 8/12/22.
 //
-import Foundation
 
 // Used to test commands and etc.
 // practice file
+// cannot run the other files. They are purely for pasting the solutions
+// go here to test and experiment
+struct Solution {
+    static func uncommonFromSentences(_ s1: String, _ s2: String) -> [String] {
+        let sentence1: Set = Set(s1.split(separator: " "))
+        let sentence2: Set = Set(s2.split(separator: " "))
+        //        return Array(sentence1.symmetricDifference(sentence2))
+        return Array(sentence1.symmetricDifference(sentence2))
+    }
+}
 
-print("test")
+print(Solution.uncommonFromSentences("this apple is sweet", "this apple is sour"))
